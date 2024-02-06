11444bec16bd27b0b6a2e428da63ddf2f67a8d62
mosyamosya <93034139+mosyamosya@users.noreply.github.com>
2024-02-06 13:59:49 +0200

    init

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+export function Footer() {
+    return (
+        <footer className='bg-black text-white'>
+            <div className='max-w-[1240px] mx-auto py-2 text-sm sm:text-base'>
+                <div className='flex justify-center'>
+                    
+                </div>
+                <div className='flex justify-center flex-col items-center'>
+                    <p className='text-center'>© Copyright 2024 React Shop | All Rights Reserved</p>
+                    
+                        Created by Myroslava Dubinetska
+                </div>
+            </div>
+        </footer>
+    )
+}
